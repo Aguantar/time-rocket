a7795048d3084a7d02dbb59dcd67a0cd2342c1c5
Aguantar <aguantar7093@gmail.com>
2025-03-24 16:12:01 +0900

    init

---
+++ src/main/java/com/tr/timerocket/TimeRocketApplication.java
@@ -0,0 +1,13 @@
+package com.tr.timerocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeRocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeRocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tr/timerocket/TimeRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.timerocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeRocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
